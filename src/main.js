18568257d0926160c75ac34d3e0f6459229a362a
Tan Wei Ming <TANWEIMING00@GMAIL.COM>
2023-03-19 14:20:29 +0800

    adding dependencies

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import bootstrap from 'bootstrap';
 
 import './assets/main.css'
 
@@ -48,4 +49,4 @@ const vuetify = createVuetify({
   // },
 })
 
-createApp(App).use(vuetify).use(router).mount('#app')
+createApp(App).use(vuetify).use(router).use(bootstrap).mount('#app')
