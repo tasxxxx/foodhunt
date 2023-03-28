9caff3a4ed86ff07626de95b78514066473a7215
Tan Wei Ming <TANWEIMING00@GMAIL.COM>
2023-03-29 01:25:26 +0800

    Update Restaurant.vue

--- src/views/Restaurant.vue
+++ src/views/Restaurant.vue
@@ -1,104 +1,127 @@
 <template>  
-    <NavigationBar1/>
-    <div class="restaurant-info">
-      <img id = "restaurantimg" src="@/assets/mcdonalds.jpg" alt = "">
-      <img id="restaurantimg1" :src="restaurant.imageurl" alt="">
-      <h1 id="restaurantname">{{ restaurant.name }}</h1>       
-      <h3 id="restauranttag" v-html="restaurant.tags"></h3>
-      <div class="text-right">
-        <v-dialog v-model="dialog" width="auto"> 
-          <template v-slot:activator="{ props }">
-              <v-btn
-                color="rgba(109,93,36,1)"
-                v-bind="props"
-                prepend-icon="mdi-information" 
-                variant="tonal"
-                style="font-family:Nunito"
-              >
-              View Info
-              </v-btn>
-          </template>
-            <v-card>
-              <v-card-text>
-                <h2 class="text-center" style="font-family:Nunito">About </h2>
-                <table>
-                  <thead>
-                  </thead>
-                    <tr class="text-left" style="font-family:Mulish"> 
-                      Address
-                      <td style="font-family:Mulish">{{ restaurant.address }}</td>
-                    </tr>
-                  <h3 style="font-family:Nunito; text-align: center">Operating Hours </h3>
-                    <tr class="text-left" style="font-family:Mulish"> 
-                      Monday
-                      <td style="font-family:Mulish">{{ restaurant.monday }}</td>
-                    </tr>
-                    <tr class="text-left" style="font-family:Mulish"> 
-                      Tuesday
-                      <td style="font-family:Mulish">{{ restaurant.tuesday }}</td>
-                    </tr>
-                    <tr class="text-left" style="font-family:Mulish"> 
-                      Wednesday
-                      <td style="font-family:Mulish">{{ restaurant.wednesday }}</td>
-                    </tr>
-                    <tr class="text-left" style="font-family:Mulish"> 
-                      Thursday
-                      <td style="font-family:Mulish">{{ restaurant.thursday }}</td>
-                    </tr>
-                    <tr class="text-left" style="font-family:Mulish"> 
-                      Friday
-                      <td style="font-family:Mulish">{{ restaurant.friday }}</td>
-                    </tr>
-                    <tr class="text-left" style="font-family:Mulish"> 
-                      Saturday
-                      <td style="font-family:Mulish">{{ restaurant.saturday }}</td>
-                    </tr>
-                    <tr class="text-left" style="font-family:Mulish"> 
-                      Sunday
-                      <td style="font-family:Mulish">{{ restaurant.sunday }}</td>
-                    </tr>
-                    <tr class="text-left" style="font-family:Mulish"> 
-                      Remarks
-                      <td style="font-family:Mulish" v-html="restaurant.remarks"></td>
-                    </tr>
-                </table>
-            </v-card-text>
-              <v-card-actions>
-              <v-btn color="rgba(109,93,36,1)" block @click="dialog = false">Close</v-btn>
-              </v-card-actions>
-              </v-card>
-            </v-dialog>
-          </div>
-      </div> 
+  <NavigationBar1/>
+  <div class="restaurant-info">
+    <img id = "restaurantimg" src="@/assets/mcdonalds.jpg" alt = "">
+    <img id="restaurantimg1" :src="restaurant.imageurl" alt="">
+    <h1 id="restaurantname">{{ restaurant.name }}</h1>       
+    <h3 id="restauranttag" v-html="restaurant.tags"></h3>
+    <div class="text-right">
+      <v-dialog v-model="dialog" width="auto"> 
+        <template v-slot:activator="{ props }">
+            <v-btn
+              color="rgba(109,93,36,1)"
+              v-bind="props"
+              prepend-icon="mdi-information" 
+              variant="tonal"
+              style="font-family:Nunito"
+            >
+            View Info
+            </v-btn>
+        </template>
+          <v-card>
+            <v-card-text>
+              <h2 class="text-center" style="font-family:Nunito">About </h2>
+              <table>
+                <thead>
+                </thead>
+                  <tr class="text-left" style="font-family:Mulish"> 
+                    Address
+                    <td style="font-family:Mulish">{{ restaurant.address }}</td>
+                  </tr>
+                <h3 style="font-family:Nunito; text-align: center">Operating Hours </h3>
+                  <tr class="text-left" style="font-family:Mulish"> 
+                    Monday
+                    <td style="font-family:Mulish">{{ restaurant.monday }}</td>
+                  </tr>
+                  <tr class="text-left" style="font-family:Mulish"> 
+                    Tuesday
+                    <td style="font-family:Mulish">{{ restaurant.tuesday }}</td>
+                  </tr>
+                  <tr class="text-left" style="font-family:Mulish"> 
+                    Wednesday
+                    <td style="font-family:Mulish">{{ restaurant.wednesday }}</td>
+                  </tr>
+                  <tr class="text-left" style="font-family:Mulish"> 
+                    Thursday
+                    <td style="font-family:Mulish">{{ restaurant.thursday }}</td>
+                  </tr>
+                  <tr class="text-left" style="font-family:Mulish"> 
+                    Friday
+                    <td style="font-family:Mulish">{{ restaurant.friday }}</td>
+                  </tr>
+                  <tr class="text-left" style="font-family:Mulish"> 
+                    Saturday
+                    <td style="font-family:Mulish">{{ restaurant.saturday }}</td>
+                  </tr>
+                  <tr class="text-left" style="font-family:Mulish"> 
+                    Sunday
+                    <td style="font-family:Mulish">{{ restaurant.sunday }}</td>
+                  </tr>
+                  <tr class="text-left" style="font-family:Mulish"> 
+                    Remarks
+                    <td style="font-family:Mulish" v-html="restaurant.remarks"></td>
+                  </tr>
+              </table>
+          </v-card-text>
+            <v-card-actions>
+            <v-btn color="rgba(109,93,36,1)" block @click="dialog = false">Close</v-btn>
+            </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </div>
+    </div> 
 
-    <h1 class="text" style="font-family:Nunito">Available Products</h1>
-    <div class="category-wrapper">
-      <div v-for="(products, category) in categorizedProducts" :key="category" class="category">
-    <h2 id="cat">{{ category }}</h2>
-    <div class="product-wrapper">
-      <div v-for="prod in products" :key="prod.name" class="product">
-        <img src="@/assets/mcdonalds.jpg" alt="Restaurant Image" class="product-img">
-        <div class="product-name">
-          {{ prod.Name }}
+<h1 class="text" style="font-family:Nunito">Available Products</h1>  
+<v-card id="finally">
+  <v-tabs
+    v-model="tab"
+    background-color="primary"
+  >
+    <v-tab
+      v-for="(products, category) in categorizedProducts"
+      :key="category"
+      :value="category"
+    >
+      {{ category }}
+    </v-tab>
+  </v-tabs>
+
+  <v-card-text>
+    <v-window v-model="tab">
+      <v-window-item
+        v-for="(products, category) in categorizedProducts"
+        :key="category"
+        :value="category"
+      >
+        <div class="category-wrapper">
+          <div class="category">
+            <div class="product-wrapper">
+              <div v-for="prod in products" :key="prod.name" class="product">
+                <img src="@/assets/mcdonalds.jpg" alt="Restaurant Image" class="product-img">
+                <div class="product-name">
+                  {{ prod.Name }}
+                </div>
+                <div class="description"> 
+                  {{ prod.Description }}
+                </div>
+                <div class="addedQty"> 
+                  <label for="quantity">Select Quantity: </label>
+                  <select v-model="prod.selectedQty">
+                    <option v-for="i in prod.AvailableQty" :key="i" :value="i">{{ i }}</option>
+                  </select>          
+                </div>
+                <div class="price"> 
+                  Price: ${{ prod.Price }}
+                </div>
+                <add-to-cart id="ATC" :quantity="prod.selectedQty" :prodID="prod"></add-to-cart>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="description"> 
-          {{ prod.Description }}
-        </div>
-        <div class="addedQty"> 
-          <label for="quantity">Select Quantity: </label>
-          <select v-model="prod.selectedQty">
-            <option v-for="i in prod.AvailableQty" :key="i" :value="i">{{ i }}</option>
-          </select>          
-        </div>
-        <div class="price"> 
-          Price: ${{ prod.Price }}
-        </div>
-        <add-to-cart id="ATC" :quantity = "prod.selectedQty" :prodID= "prod"></add-to-cart>
-      </div>
-    </div>
-  </div>
-  <div class="clear"></div>
-</div>
+      </v-window-item>
+    </v-window>
+  </v-card-text>
+</v-card>
 </template>
 
 <script> 
@@ -110,175 +133,199 @@ import { getDoc, doc, getDocs, collection} from 'firebase/firestore';
 const db = getFirestore(firebaseApp);
 
 export default {
-  name: "Restaurant",
-  // props: ['id'],
-  components:{
-    NavigationBar1,
-    AddToCart
-  }, 
-  data() {
-    return {
-      dialog: false, 
-      restaurant: [],
-      products: [],
-      imageurl: "",
+name: "Restaurant",
+// props: ['id'],
+components:{
+  NavigationBar1,
+  AddToCart
+}, 
+data() {
+  return {
+    dialog: false, 
+    restaurant: [],
+    products: [],
+    uniqueCategories: [], 
+    imageurl: "",
+
+    tab: ""
+  }
+},  
+async mounted() {
+  // for the time being it is hardcoded, but i will need to wait for the router to be ready before I can... 
+  const restaurantRef = doc(db, 'restaurant_personalisation', 'McDonalds');
+  const docSnap = await getDoc(restaurantRef);
+
+  if (docSnap.exists()) {
+    const documentData = docSnap.data();
+
+    const name = documentData.Name;
+    // const image = documentData.imageID;
+    const tags = documentData.Tags.join(" / ");
+    const address = documentData.Address;
+    const monday = documentData.Monday; 
+    const tuesday = documentData.Tuesday; 
+    const wednesday = documentData.Wednesday; 
+    const thursday = documentData.Thursday; 
+    const friday = documentData.Friday; 
+    const saturday = documentData.Saturday; 
+    const sunday = documentData.Sunday; 
+    const remarks = documentData.Remarks.join("<br>");
+    this.restaurant = {
+      name,
+      tags,
+      address,
+      monday,
+      tuesday, 
+      wednesday,
+      thursday,
+      friday, 
+      saturday, 
+      sunday, 
+      remarks,
+    };
+    this.imageurl = documentData.imageID;
+  } else {
+    console.log('No such document!');
+  }
+
+  const productRef = await getDocs(collection(db, "food_listings"));
+  productRef.forEach((doc) => {
+    if (doc.data().Vendor === 'McDonalds') {
+      const product = doc.data();
+      product.category = doc.data().Category;
+      this.products.push(product);
     }
-  },  
-  async mounted() {
-    // for the time being it is hardcoded, but i will need to wait for the router to be ready before I can... 
-    const restaurantRef = doc(db, 'restaurant_personalisation', 'McDonalds');
-    const docSnap = await getDoc(restaurantRef);
-
-    if (docSnap.exists()) {
-      const documentData = docSnap.data();
-
-      const name = documentData.Name;
-      // const image = documentData.imageID;
-      const tags = documentData.Tags.join(" / ");
-      const address = documentData.Address;
-      const monday = documentData.Monday; 
-      const tuesday = documentData.Tuesday; 
-      const wednesday = documentData.Wednesday; 
-      const thursday = documentData.Thursday; 
-      const friday = documentData.Friday; 
-      const saturday = documentData.Saturday; 
-      const sunday = documentData.Sunday; 
-      const remarks = documentData.Remarks.join("<br>");
-      this.restaurant = {
-        name,
-        tags,
-        address,
-        monday,
-        tuesday, 
-        wednesday,
-        thursday,
-        friday, 
-        saturday, 
-        sunday, 
-        remarks,
-      };
-      this.imageurl = documentData.imageID;
-    } else {
-      console.log('No such document!');
-    }
-
-    const productRef = await getDocs(collection(db, "food_listings"));
-    productRef.forEach((doc) => {
-      if (doc.data().Vendor === 'McDonalds') {
-        const product = doc.data();
-        product.category = doc.data().Category;
-        this.products.push(product);
+  });
+  },
+  
+  computed: {
+  categorizedProducts() {
+    return this.products.reduce((acc, prod) => {
+      if (!acc[prod.category]) {
+        acc[prod.category] = [prod]
+      } else {
+        acc[prod.category].push(prod)
       }
-    });
-    }, 
-    computed: {
-    categorizedProducts() {
-      return this.products.reduce((acc, prod) => {
-        if (!acc[prod.category]) {
-          acc[prod.category] = [prod]
-        } else {
-          acc[prod.category].push(prod)
-        }
-        return acc
-      }, {})
+      return acc
+    }, {})
+  }
+}, 
+async created() {
+  const productRef = await getDocs(collection(db, "food_listings"));
+  const uniqueCategories = [];
+  
+  productRef.forEach((doc) => {
+    if (doc.data().Vendor === 'McDonalds') {
+      const product = doc.data();
+      product.category = doc.data().Category;
+      if (!uniqueCategories.includes(product.category)) {
+        uniqueCategories.push(product.category);
+      }
     }
-  }, 
+  });  
+  this.tab = uniqueCategories[0]; // set default tab selected to first category
+}
 }
 </script>
 <style scoped>
 
 #restaurantimg {
-  width: 100vw;
-  height: 35vh;
+width: 100vw;
+height: 35vh;
 }
 #restaurantname {
-  margin-left:5vw;
-  font-family:"Nunito";
-}
-
-.category-wrapper {
-  margin-left:75px;
+margin-left:5vw;
+font-family:"Nunito";
 }
 
 #restauranttag {
-  margin-left:5vw;
-  font-family:"Lato";
+margin-left:5vw;
+font-family:"Lato";
 }
 .text-right {
-  margin-top:-65px;
-  margin-right:5vw;
+margin-top:-65px;
+margin-right:5vw;
 }
 
 .category:nth-child(3n+1) {
-  clear: both;
+clear: both;
 }
 
 #cat {
-  padding-left:20px;
-  font-family:Nunito;
+padding-left:20px;
+font-family:Nunito;
 }
 
 .text { 
-  margin-left:5vw;
-  margin-top: 50px;
+margin-left:5vw;
+margin-top: 35px;
 }
 .product-wrapper {
-  display: flex;
-  flex-wrap: wrap;
+display: flex;
+flex-wrap: wrap;
 }
 .product{
-  border: 2px solid #d3cdcd;
-  width: 300px;
-  height: 260px;
-  box-shadow: 0 2px #d3cdcd;
-  border-radius: 15px;
-  position: relative;
-  margin: 20px;
+border: 2px solid #d3cdcd;
+width: 300px;
+height: 260px;
+box-shadow: 0 2px #d3cdcd;
+border-radius: 15px;
+position: relative;
+margin: 20px;
 } 
 
+#finally {
+width:90vw;
+margin-left: 5vw;
+}
+
 .product-name {
-  font-size: 20px;
-  margin-top: -8px;
-  margin-left: 5px;
-  font-family:"Lato";
+font-size: 22px;
+margin-left: 5px;
+font-family:"Lato";
 
 }
 .product-img {
-  width: 100%;
-  height: 150px;
-  border-radius: 15px;
-  font-family:"Lato";
+width: 100%;
+height: 150px;
+border-radius: 15px;
+font-family:"Lato";
 }
 
 .description{
-  width: 99999999999999px;
-  margin-left: 5px;
-  font-family:"Lato";
+width: 99999999999999px;
+font-size: 16px;
+margin-top: 2.5px;
+margin-left: 5px;
+font-family:"Lato";
 }
 .addedQty{
-  width: 99999999999999px;
-  margin-left: 5px;
-  font-family:"Lato";
+width: 99999999999999px;
+font-size: 16px;
+margin-top: 2.5px;
+margin-left: 5px;
+font-family:"Lato";
 }
 
 .price{
-  width: 99999999999999px;
-  margin-left: 5px;
-  font-family:"Lato";
+width: 99999999999999px;
+font-size: 16px;
+margin-top: 2.5px;
+margin-left: 5px;
+font-family:"Lato";
 }
 
 #ATC{
-  left: 170px;
-  margin-top:-60px;
-  height: 10%;
-  width: 42%;
+left: 170px;
+margin-top:-40px;
+height: 10%;
+width: 42%;
 }
 
 select {
-  -webkit-appearance: listbox !important;
+-webkit-appearance: listbox !important;
 }
-.clear {
-  clear: both;
-}
+
 </style>
+
 
