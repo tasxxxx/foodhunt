857130101a2a96fccecd225e99475a2cdb281e4b
Jia Hui <jiahuitan36@gmail.com>
2023-04-01 15:35:10 +0800

    changed restaurant listing page

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,25 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Landing from '../views/Landing.vue'
-import CustRegister from '../views/CustRegister.vue'
-import Login from '../views/Login.vue'
-import MainListing from '../views/MainListing.vue'
-import Test from '../views/Test.vue'
-import Test2 from '../views/Test2.vue'
-import CustProfile from '../views/CustProfile.vue'
-import PartnerUs from '../views/PartnerUs.vue'
-import MyReservation from '../views/MyReservation.vue'
-import Restaurant from '../views/Restaurant.vue'
-import MyCart from '../views/MyCart.vue'
-import VendorDashboard from '../views/VendorDashboard.vue'
-import CreateListing from '../views/CreateListing.vue'
-import VendorPersonalisation from '../views/VendorPersonalisation.vue'
-import VendorRegister from '../views/VendorRegister.vue'
-import VendorProfileEditOne from '../views/VendorProfileEditOne.vue'
-import VendorProfileEditTwo from '../views/VendorProfileEditTwo.vue'
-import RestaurantListing from '../views/RestaurantListing.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import Landing from '../views/Landing.vue';
+import CustRegister from '../views/CustRegister.vue';
+import Login from '../views/Login.vue';
+import MainListing from '../views/MainListing.vue';
+import Test from '../views/Test.vue';
+import Test2 from '../views/Test2.vue';
+import CustProfile from '../views/CustProfile.vue';
+import PartnerUs from '../views/PartnerUs.vue';
+import MyReservation from '../views/MyReservation.vue';
+import Restaurant from '../views/Restaurant.vue';
+import MyCart from '../views/MyCart.vue';
+import VendorDashboard from '../views/VendorDashboard.vue';
+import CreateListing from '../views/CreateListing.vue';
+import VendorPersonalisation from '../views/VendorPersonalisation.vue';
+import VendorRegister from '../views/VendorRegister.vue';
+import VendorProfileEditOne from '../views/VendorProfileEditOne.vue';
+import VendorProfileEditTwo from '../views/VendorProfileEditTwo.vue';
+import RestaurantListing from '../views/RestaurantListing2.vue';
 import VendorListing from '../views/VendorListing.vue';
 import Confirmation from '../views/confirmation.vue';
 import VendorReservation from '../views/VendorReservation.vue';
-
 
 const routes = [
   {
@@ -29,11 +28,12 @@ const routes = [
     component: Landing,
   },
   {
-    // path: '/restaurants/:id',
-    path: '/restaurants',
+    // path: '/restrauants/:id',
+    path: '/restaurant/:id',
+    // path: '/restaurants',
     name: 'restaurant',
     component: Restaurant,
-    // props: true
+    props: true,
   },
   {
     path: '/partnerus',
@@ -88,50 +88,50 @@ const routes = [
   {
     path: '/vendor-dashboard',
     name: 'vendor-dashboard',
-    component: VendorDashboard
+    component: VendorDashboard,
   },
   {
     path: '/create-listing',
     name: 'create-listing',
-    component: CreateListing
+    component: CreateListing,
   },
   {
     path: '/vendor-personalisation',
     name: 'vendor-personalisation',
-    component: VendorPersonalisation
+    component: VendorPersonalisation,
   },
   {
     path: '/vendor-register',
     name: 'vendor-register',
-    component: VendorRegister
+    component: VendorRegister,
   },
   {
     path: '/vendor-profileeditone',
     name: 'vendor-profileeditone',
-    component: VendorProfileEditOne
+    component: VendorProfileEditOne,
   },
   {
     path: '/vendor-profileedittwo',
     name: 'vendor-profileedittwo',
-    component: VendorProfileEditTwo
+    component: VendorProfileEditTwo,
   },
   {
     path: '/vendor-listing',
     name: 'vendor-listing',
-    component: VendorListing
+    component: VendorListing,
   },
   {
     path: '/confirmation/:reservationNumber',
     name: 'Confirmation',
     component: Confirmation,
-    props: true
+    props: true,
   },
   {
     path: '/vendor-reservation',
     name: 'vendor-reservation',
     component: VendorReservation,
   },
-]
+];
 
 const router = createRouter({
   mode: 'history',
