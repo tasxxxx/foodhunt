47416c61b8070ae998915ba0ed3aafa5a7694c88
tasneembenazir <e0772937@u.nus.edu>
2023-04-02 16:13:30 +0800

    Update SearchBar.vue

--- src/components/icons/SearchBar.vue
+++ src/components/icons/SearchBar.vue
@@ -4,17 +4,11 @@
         label= "Search"
         variant="outlined"
         append-inner-icon="mdi-magnify"
-<<<<<<< Updated upstream
-=======
-        append-icon="mdi-filter"
-        @click:append="openMenu = true"
->>>>>>> Stashed changes
         single-line
         style="font-family:Nunito"
         density="compact"
         class="search"
         @change="onChange"
-<<<<<<< Updated upstream
         @keyup="onChange"
     >
     <template v-slot:append>
@@ -26,27 +20,6 @@
         open-on-hover
     >
     </v-menu>
-=======
-        @keyup="onChange">
-    >
-
-    <v-menu v-model="openMenu" offset-y>
-        <v-list>
-            <v-list-item v-for="item in items" :key="item.id" @click="selectItem(item)">
-                <v-list-item-title>{{ item.title }}</v-list-item-title>
-            </v-list-item>
-      </v-list>
-    </v-menu>
-
-
-        <!-- <v-menu open-on-hover>
-        <template v-slot:activator="{ props }">
-        <template v-slot:append>
-            <v-icon class="filterIcon" v-bind="props">{{ icon }}</v-icon>
-        </template> -->
-    </v-text-field>
-
->>>>>>> Stashed changes
 </template>
 
 <script>
@@ -55,15 +28,6 @@ export default {
         return {
             searchText: '',
             icon: "mdi-filter",
-<<<<<<< Updated upstream
-=======
-            openMenu: false,
-            items: [
-                { id: 1, title: 'Item 1' },
-                { id: 2, title: 'Item 2' },
-                { id: 3, title: 'Item 3' }
-            ],
->>>>>>> Stashed changes
 
         }
     },
@@ -73,16 +37,7 @@ export default {
     methods: {
         onChange() {
             this.$emit("search", this.searchText)
-<<<<<<< Updated upstream
         }
-=======
-        },
-
-        selectItem(item) {
-            this.text = item.title;
-            this.showMenu = false;
-        },
->>>>>>> Stashed changes
     }
 }
 </script>
