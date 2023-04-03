a4a87451ccc4027d2d7bb8441bb220a54e22bc54
Tan Wei Ming <TANWEIMING00@GMAIL.COM>
2023-04-04 00:17:24 +0800

    changes to code logic
    done for the day goodnight 3103

--- src/components/AddToCart.vue
+++ src/components/AddToCart.vue
@@ -16,7 +16,8 @@
           return {
               user: false,
               maxQuantity: 0, 
-              currentCartQty: 0
+              currentCartQty: 0,
+              tempCart: []
           }
     },
 
@@ -66,59 +67,86 @@
                 // If the document does not exist, create it
                 await setDoc(cartRef, { products: {}});
               }
-                
+              
+              
               // Get the current list of products in the cart, or create an empty list if none exists
               const products = cartData.exists() ? cartData.data().products : {};
-              
-              const productKey = `${this.prodID.Food_listingID},${this.prodID.VendorName},${this.prodID.Name},${this.prodID.Price}`
 
-              // get maxQuantity 
-                const productRef = await getDocs(collection(db, "food_listings"));
-                productRef.forEach((doc) => {
-                if (this.prodID.Food_listingID === doc.data().Food_listingID) {
-                  this.maxQuantity = doc.data().AvailableQty
-                }})
+              this.tempCart = null;
 
-                if(this.maxQuantity < (products[productKey] + this.quantity)) {
-                  toast.error("Unsuccessful execution! Please reduce quantity!", {
-                  position: "top-right",
-                  timeout: 2019,
-                  closeOnClick: true,
-                  pauseOnFocusLoss: false,
-                  pauseOnHover: false,
-                  draggable: true,
-                  draggablePercent: 2,
-                  showCloseButtonOnHover: false,
-                  hideProgressBar: false,
-                  closeButton: "button",
-                  icon: true,
-                  rtl: false
-                  });   
-                } else {
-                  if (products.hasOwnProperty([this.prodID.Food_listingID, this.prodID.VendorName, this.prodID.Name, this.prodID.Price])) {
-                  products[[this.prodID.Food_listingID, this.prodID.VendorName, this.prodID.Name, this.prodID.Price]] += this.quantity;
+              this.tempCart = Object.entries(products).map(async ([key, value]) => {
+                const [foodID, restaurant, item, price] = key.split(",");
+                return restaurant;
+              });
+              // sort the cart by id
+              this.tempCart = await Promise.all(this.tempCart);
+              console.log(this.tempCart)
+            
+              if (this.tempCart.length === 0 || (this.tempCart[0] === this.prodID.VendorName)) {
+                const productKey = `${this.prodID.Food_listingID},${this.prodID.VendorName},${this.prodID.Name},${this.prodID.Price}`
+                // get maxQuantity 
+                  const productRef = await getDocs(collection(db, "food_listings"));
+                  productRef.forEach((doc) => {
+                  if (this.prodID.Food_listingID === doc.data().Food_listingID) {
+                    this.maxQuantity = doc.data().AvailableQty
+                  }})
+
+                  if(this.maxQuantity < (products[productKey] + this.quantity)) {
+                    toast.error("Unsuccessful execution! Please reduce quantity!", {
+                    position: "top-right",
+                    timeout: 2019,
+                    closeOnClick: true,
+                    pauseOnFocusLoss: false,
+                    pauseOnHover: false,
+                    draggable: true,
+                    draggablePercent: 2,
+                    showCloseButtonOnHover: false,
+                    hideProgressBar: false,
+                    closeButton: "button",
+                    icon: true,
+                    rtl: false
+                    });   
                   } else {
-                  products[[this.prodID.Food_listingID, this.prodID.VendorName, this.prodID.Name, this.prodID.Price]] = this.quantity;
+                    if (products.hasOwnProperty([this.prodID.Food_listingID, this.prodID.VendorName, this.prodID.Name, this.prodID.Price])) {
+                    products[[this.prodID.Food_listingID, this.prodID.VendorName, this.prodID.Name, this.prodID.Price]] += this.quantity;
+                    } else {
+                    products[[this.prodID.Food_listingID, this.prodID.VendorName, this.prodID.Name, this.prodID.Price]] = this.quantity;
+                    }
+                    await setDoc(cartRef, { products: products });
+                    
+                    toast.success("Product is successfully added to the cart!", {
+                    position: "top-right",
+                    timeout: 2019,
+                    closeOnClick: true,
+                    pauseOnFocusLoss: false,
+                    pauseOnHover: false,
+                    draggable: true,
+                    draggablePercent: 2,
+                    showCloseButtonOnHover: false,
+                    hideProgressBar: false,
+                    closeButton: "button",
+                    icon: true,
+                    rtl: false
+                    }); 
                   }
-                  await setDoc(cartRef, { products: products });
-                  
-                  toast.success("Product is successfully added to the cart!", {
-                  position: "top-right",
-                  timeout: 2019,
-                  closeOnClick: true,
-                  pauseOnFocusLoss: false,
-                  pauseOnHover: false,
-                  draggable: true,
-                  draggablePercent: 2,
-                  showCloseButtonOnHover: false,
-                  hideProgressBar: false,
-                  closeButton: "button",
-                  icon: true,
-                  rtl: false
-                  }); 
+                } else {
+                    toast.error("You are not allowed to add items from multiple vendors!", {
+                    position: "top-right",
+                    timeout: 2019,
+                    closeOnClick: true,
+                    pauseOnFocusLoss: false,
+                    pauseOnHover: false,
+                    draggable: true,
+                    draggablePercent: 2,
+                    showCloseButtonOnHover: false,
+                    hideProgressBar: false,
+                    closeButton: "button",
+                    icon: true,
+                    rtl: false
+                    });  
                 }
-            }
-            } 
+                } 
+        }
         else { 
           //// if user is not logged in, and when it clicks on the add to cart
           //// error button will be prompted and push to login. 
