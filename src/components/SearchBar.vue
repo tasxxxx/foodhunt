cb3b7cf6429a36f77b57f60e07665941e1f6fe2f
tasneembenazir <e0772937@u.nus.edu>
2023-04-03 14:13:31 +0800

    Revert "Update SearchBar.vue"
    This reverts commit 2b66460fb4b7a11b8c5afe6cfa15824e932137d4.

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -13,11 +13,7 @@
         @keyup="onChange"
     />
 
-    <v-menu 
-    open-on-click
-    :close-on-content-click="false"
-    location="end"
-    >
+    <v-menu open-on-hover>
         <template v-slot:activator="{ props }">
           <v-col class="m-0">
           <v-btn
@@ -29,109 +25,20 @@
           </v-btn>
         </v-col>
         </template>
-
-    <v-card
-      class="mx-auto"
-      max-width="300"
-    >
-      <v-toolbar
-        flat
-        color="primary"
-        dark
-      >
-
-        <v-toolbar-title style="font-family:Nunito">Tags</v-toolbar-title>
-      </v-toolbar>
   
-        <v-card-text>
-        <h2 class="text-h6 mb-2">
-          Cuisines
-        </h2>
-  
-        <v-chip-group
-          v-model="cuisines"
-          column
-          multiple
-        >
-          <v-chip
-            filter
-            variant="outlined"
-          >
-            Chinese
-          </v-chip>
-          <v-chip
-            filter
-            variant="outlined"
-          >
-            Indian
-          </v-chip>
-          <v-chip
-            filter
-            variant="outlined"
-          >
-            Malay
-          </v-chip>
-          <v-chip
-            filter
-            variant="outlined"
-          >
-            Café
-          </v-chip>
-        </v-chip-group>
-      </v-card-text>
-  
-      <v-card-text>
-        <h2 class="text-h6 mb-2">
-          Price range
-        </h2>
-  
-        <v-chip-group
-          v-model="pricerange"
-          column
-          multiple
-        >
-          <v-chip
-            filter
-            variant="outlined"
-          >
-            $
-          </v-chip>
-          <v-chip
-            filter
-            variant="outlined"
-          >
-            $$
-          </v-chip>
-          <v-chip
-            filter
-            variant="outlined"
-          >
-            $$$
-          </v-chip>
-        </v-chip-group>
-      </v-card-text>
-    </v-card>
-
-
-        <!-- <v-card>
-          <v-card-title>My Card Title</v-card-title>
-          <v-card-text>Some text for the card</v-card-text>
-        </v-card> -->
-  
-        <!-- <v-list>
+        <v-list>
           <v-list-item
             v-for="(item, index) in items"
             :key="index"
           >
             <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item>
-        </v-list> -->
+        </v-list>
       </v-menu>
 </div>
 </template>
 
 <script>
-import { computed } from 'vue'
 export default {
     data() {
         return {
@@ -141,9 +48,7 @@ export default {
                 { title: 'Click Me' },
                 { title: 'Click Me' },
                 { title: 'Click Me 2' },
-                ],
-            cuisines: [],
-            pricerange: [],
+            ],
 
         }
     },
@@ -154,8 +59,7 @@ export default {
         onChange() {
             this.$emit("search", this.searchText)
         },
-
-    },
+    }
 }
 </script>
 
